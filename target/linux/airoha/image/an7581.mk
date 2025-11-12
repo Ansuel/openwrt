c0e113555da6ce0f714ddecad81b321998867459
Christian Marangi <ansuelsmth@gmail.com>
2025-11-12 15:28:22 +0100

    airoha: add AN8811 RFB variant
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -32,6 +32,13 @@ define Device/airoha_an7581-evb
 endef
 TARGET_DEVICES += airoha_an7581-evb
 
+define Device/airoha_an7581-evb-an8811
+  $(call Device/airoha_an7581-evb)
+  DEVICE_MODEL := AN7581 Evaluation Board (SNAND + AN8811)
+  DEVICE_DTS := an7581-evb-an8811
+endef
+TARGET_DEVICES += airoha_an7581-evb-an8811
+
 define Device/airoha_an7581-evb-emmc
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7581 Evaluation Board (EMMC)
